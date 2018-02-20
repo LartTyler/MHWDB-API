b3d7d4a6ab8dcd7198db38051ed355b910b566ee
Tyler Lartonoix <larttyler@live.com>
2018-02-20 17:30:50 -0500

    Add Doctrine and Doze

--- config/bundles.php
+++ config/bundles.php
@@ -1,4 +1,10 @@
 <?php
-	return [
-		Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-	];
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+	DaybreakStudios\DozeBundle\DaybreakStudiosDozeBundle::class => ['all' => true],
+];
