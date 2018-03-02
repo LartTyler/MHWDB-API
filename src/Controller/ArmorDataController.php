28d987d80fca228024fa27a76183913a19f0ecb1
Tyler Lartonoix <larttyler@live.com>
2018-03-02 13:44:06 -0500

    Add controller and routes for armor

---
+++ src/Controller/ArmorDataController.php
@@ -0,0 +1,20 @@
+<?php
+	namespace App\Controller;
+
+	use App\Entity\Armor;
+	use DaybreakStudios\DozeBundle\ResponderService;
+	use Symfony\Bridge\Doctrine\RegistryInterface;
+	use Symfony\Component\Routing\RouterInterface;
+
+	class ArmorDataController extends AbstractDataController {
+		/**
+		 * ArmorDataController constructor.
+		 *
+		 * @param RegistryInterface $doctrine
+		 * @param ResponderService  $responder
+		 * @param RouterInterface   $router
+		 */
+		public function __construct(RegistryInterface $doctrine, ResponderService $responder, RouterInterface $router) {
+			parent::__construct($doctrine, $responder, $router, Armor::class);
+		}
+	}
