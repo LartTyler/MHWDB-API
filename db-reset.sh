587c6d06e4489510ab8c930fbe2dfb1c06e8b9e4
Tyler Lartonoix <larttyler@live.com>
2018-04-18 10:01:33 -0400

    Update db-reset.sh to be more user friendly

--- db-reset.sh
+++ db-reset.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 function usage() {
-    echo "${0} SNAPSHOT [DB]"
+    echo "${0} snapshot [database]"
+    echo
+    echo "snapshot - a path to a SQL file to load, or the string 'latest' to load the most recent file from the"
+    echo "           snapshots directory"
+    echo "database - the schema to load the SQL file into"
 
     exit
 }
 
-if [[ $# < 1 ]]; then
+
+
+if [[ $# < 1  || "${1}" == '--help' || "${1}" == '-h' ]]; then
     usage
 fi
 
 snapshot="${1}"
+
+if [[ "${snapshot}" == "latest" ]]; then
+    snapshot=`ls -t snapshots | cut -f1 | head -n1`
+    snapshot="snapshots/${snapshot}"
+fi
+
 db="application"
 
 if [[ $# > 1 ]]; then
