9043f574cb523696295e8f7efb45190cc98a6fdf
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-05-09 11:53:32 -0400

    Remove profiler

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     DaybreakStudios\DozeBundle\DaybreakStudiosDozeBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
