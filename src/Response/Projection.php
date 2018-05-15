3e0056ab651f39458ba3b03b52be00707e121e68
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-05-15 01:09:19 -0400

    Add basic filtering logic

--- src/Response/Projection.php
+++ src/Response/Projection.php
@@ -13,6 +13,11 @@
 		protected $include;
 
 		/**
+		 * @var bool[]
+		 */
+		protected $parents = [];
+
+		/**
 		 * Projection constructor.
 		 *
 		 * @param bool[] $fields
@@ -20,6 +25,16 @@
 		public function __construct(array $fields) {
 			$this->fields = $fields;
 			$this->include = reset($fields);
+
+			// Also append the parent node of any child fields
+			foreach (array_keys($fields) as $field) {
+				$pos = strrpos($field, '.');
+
+				if ($pos === false)
+					continue;
+
+				$this->parents[substr($field, 0, $pos)] = true;
+			}
 		}
 
 		/**
@@ -40,7 +55,16 @@
 
 			$found = $this->fields[$path] ?? false;
 
-			return $this->isInclude() && $found || !$found;
+			return $this->isInclude() ? $found : !$found;
+		}
+
+		/**
+		 * @param string $path
+		 *
+		 * @return bool
+		 */
+		public function isParent(string $path): bool {
+			return isset($this->parents[$path]);
 		}
 
 		/**
@@ -49,21 +73,39 @@
 		 *
 		 * @return array
 		 */
-		public function filter(array &$data, string $prefix = null): array {
+		public function filter(array $data, string $prefix = null): array {
 			if (!$this->fields)
 				return $data;
 
+			$output = [];
+
+			// TODO This whole thing works, but needs to be refined
 			foreach ($data as $key => $value) {
 				$path = ($prefix ? $prefix . '.' : '') . $key;
 
 				if (!$this->isAllowed($path)) {
-					unset($data[$key]);
+					if ($this->isParent($path)) {
+						if (is_array($value)) {
+							$count = sizeof($value);
 
-					continue;
-				} else if (is_array($value))
-					$this->filter($value, $path);
+							if ($count > 0 && isset($value[0])) {
+								foreach ($value as $index => $item)
+									$value[$index] = $this->filter($item, $path);
+							} else if ($count > 0) {
+								foreach ($value as $childKey => $item) {
+									if (!$this->isAllowed($path . '.' . $childKey))
+										continue 2;
+								}
+							}
+						} else
+							continue;
+					} else
+						continue;
+				}
+
+				$output[$key] = $value;
 			}
 
-			return $data;
+			return $output;
 		}
 	}
