6c97a6c9f69c93695b4b8db4789b11be0f259d3e
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-05-15 22:24:22 -0400

    Add projection optimizations to weapons

--- src/Controller/WeaponsDataController.php
+++ src/Controller/WeaponsDataController.php
@@ -35,34 +35,7 @@
 			if (!$entity)
 				return null;
 
-			/**
-			 * @param Collection|CraftingMaterialCost[] $costs
-			 *
-			 * @return array
-			 */
-			$materialTransformer = function(Collection $costs): array {
-				return array_map(function(CraftingMaterialCost $cost): array {
-					$item = $cost->getItem();
-
-					return [
-						'quantity' => $cost->getQuantity(),
-						'item' => [
-							'id' => $item->getId(),
-							'name' => $item->getName(),
-							'description' => $item->getDescription(),
-							'rarity' => $item->getRarity(),
-							'carryLimit' => $item->getCarryLimit(),
-							'sellPrice' => $item->getSellPrice(),
-							'buyPrice' => $item->getBuyPrice(),
-						],
-					];
-				}, $costs->toArray());
-			};
-
-			$crafting = $entity->getCrafting();
-			$assets = $entity->getAssets();
-
-			$data = [
+			$output = [
 				'id' => $entity->getId(),
 				'slug' => $entity->getSlug(),
 				'name' => $entity->getName(),
@@ -72,50 +45,124 @@
 					'display' => $entity->getAttack()->getDisplay(),
 					'raw' => $entity->getAttack()->getRaw(),
 				],
-				'slots' => array_map(function(Slot $slot): array {
+				// default to \stdClass to fix an empty array being returned instead of an empty object
+				'attributes' => $entity->getAttributes() ?: new \stdClass(),
+			];
+
+			if (WeaponType::isMelee($entity->getType()) && $projection->isAllowed('sharpness')) {
+				$sharpness = $entity->getSharpness();
+
+				$output['sharpness'] = [
+					'red' => $sharpness->getRed(),
+					'orange' => $sharpness->getOrange(),
+					'yellow' => $sharpness->getYellow(),
+					'green' => $sharpness->getGreen(),
+					'blue' => $sharpness->getBlue(),
+					'white' => $sharpness->getWhite(),
+				];
+			}
+
+			if ($projection->isAllowed('slots')) {
+				$output['slots'] = array_map(function(Slot $slot): array {
 					return [
 						'rank' => $slot->getRank(),
 					];
-				}, $entity->getSlots()->toArray()),
-				'elements' => array_map(function(WeaponElement $element): array {
+				}, $entity->getSlots()->toArray());
+			}
+
+			if ($projection->isAllowed('elements')) {
+				$output['elements'] = array_map(function(WeaponElement $element): array {
 					return [
 						'type' => $element->getType(),
 						'damage' => $element->getDamage(),
 						'hidden' => $element->isHidden(),
 					];
-				}, $entity->getElements()->toArray()),
-				// default to \stdClass to fix an empty array being returned instead of an empty object
-				'attributes' => $entity->getAttributes() ?: new \stdClass(),
-				'crafting' => $crafting ? [
-					'craftable' => $crafting->isCraftable(),
-					'previous' => $crafting->getPrevious() ? $crafting->getPrevious()->getId() : null,
-					'branches' => array_map(function(Weapon $branch): int {
-						return $branch->getId();
-					}, $crafting->getBranches()->toArray()),
-					'craftingMaterials' => call_user_func($materialTransformer, $crafting->getCraftingMaterials()),
-					'upgradeMaterials' => call_user_func($materialTransformer, $crafting->getUpgradeMaterials()),
-				] : null,
-				'assets' => $assets ? [
-					'icon' => $assets->getIcon() ? $assets->getIcon()->getUri() : null,
-					'image' => $assets->getImage() ? $assets->getImage()->getUri() : null,
-				] : null,
-			];
-
-			if (WeaponType::isMelee($entity->getType())) {
-				$sharpness = $entity->getSharpness();
-
-				$data += [
-					'sharpness' => [
-						'red' => $sharpness->getRed(),
-						'orange' => $sharpness->getOrange(),
-						'yellow' => $sharpness->getYellow(),
-						'green' => $sharpness->getGreen(),
-						'blue' => $sharpness->getBlue(),
-						'white' => $sharpness->getWhite(),
-					],
-				];
+				}, $entity->getElements()->toArray());
 			}
 
-			return $data;
+			if ($projection->isAllowed('crafting')) {
+				$crafting = $entity->getCrafting();
+
+				if ($crafting) {
+					/**
+					 * @param string                            $type
+					 * @param Collection|CraftingMaterialCost[] $costs
+					 *
+					 * @return array
+					 */
+					$transformer = function(string $type, Collection $costs) use ($projection): array {
+						return array_map(function(CraftingMaterialCost $cost) use ($projection, $type): array {
+							$output = [
+								'quantity' => $cost->getQuantity(),
+							];
+
+							if ($projection->isAllowed(sprintf('crafting.%s.item', $type))) {
+								$item = $cost->getItem();
+
+								$output['item'] = [
+									'id' => $item->getId(),
+									'name' => $item->getName(),
+									'description' => $item->getDescription(),
+									'rarity' => $item->getRarity(),
+									'carryLimit' => $item->getCarryLimit(),
+									'sellPrice' => $item->getSellPrice(),
+									'buyPrice' => $item->getBuyPrice(),
+								];
+							}
+
+							return $output;
+						}, $costs->toArray());
+					};
+
+					$output['crafting'] = [
+						'craftable' => $crafting->isCraftable(),
+					];
+
+					if ($projection->isAllowed('crafting.previous')) {
+						$previous = $crafting->getPrevious();
+
+						$output['crafting']['previous'] = $previous ? $previous->getId() : null;
+					}
+
+					if ($projection->isAllowed('crafting.branches')) {						$output['crafting']['branches'] = array_map(function(Weapon $branch): int {
+							return $branch->getId();
+						}, $crafting->getBranches()->toArray());
+					}
+
+					if ($projection->isAllowed('crafting.craftingMaterials')) {
+						$output['crafting']['craftingMaterials'] = call_user_func($transformer, 'craftingMaterials',
+							$crafting->getCraftingMaterials());
+					}
+
+					if ($projection->isAllowed('crafting.upgradeMaterials')) {
+						$output['crafting']['upgradeMaterials'] = call_user_func($transformer, 'upgradeMaterials',
+							$crafting->getUpgradeMaterials());
+					}
+				} else
+					$output['crafting'] = null;
+			}
+
+			if ($projection->isAllowed('assets')) {
+				$assets = $entity->getAssets();
+
+				if ($assets) {
+					$output['assets'] = [];
+
+					if ($projection->isAllowed('assets.icon')) {
+						$icon = $assets->getIcon();
+
+						$output['assets']['icon'] = $icon ? $icon->getUri() : null;
+					}
+
+					if ($projection->isAllowed('assets.image')) {
+						$image = $assets->getImage();
+
+						$output['assets']['image'] = $image ? $image->getUri() : null;
+					}
+				} else
+					$output['assets'] = null;
+			}
+
+			return $output;
 		}
 	}
