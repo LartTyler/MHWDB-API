eb8fb156c34ed30cae6288c2145242837585bff7
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-08-28 23:32:28 -0400

    Begin adding Monster entity

---
+++ src/Entity/Monster.php
@@ -0,0 +1,193 @@
+<?php
+	namespace App\Entity;
+
+	use App\Game\Element;
+	use App\Game\MonsterSpecies;
+	use App\Game\MonsterType;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+	use Doctrine\Common\Collections\ArrayCollection;
+	use Doctrine\Common\Collections\Collection;
+	use Doctrine\Common\Collections\Selectable;
+	use Doctrine\ORM\Mapping as ORM;
+
+	/**
+	 * @ORM\Entity()
+	 * @ORM\Table(name="monsters")
+	 *
+	 * Class Monster
+	 *
+	 * @package App\Entity
+	 */
+	class Monster implements EntityInterface {
+		use EntityTrait;
+
+		/**
+		 * @ORM\Column(type="string", length=64, unique=true)
+		 *
+		 * @var string
+		 */
+		private $name;
+
+		/**
+		 * @ORM\Column(type="string", length=32)
+		 *
+		 * @var string
+		 * @see MonsterType
+		 */
+		private $type;
+
+		/**
+		 * @ORM\Column(type="string", length=32)
+		 *
+		 * @var string
+		 * @see MonsterSpecies
+		 */
+		private $species;
+
+		/**
+		 * @ORM\ManyToMany(targetEntity="App\Entity\Ailment")
+		 * @ORM\JoinTable(name="monster_ailments")
+		 *
+		 * @var Ailment[]|Collection|Selectable
+		 */
+		private $ailments;
+
+		/**
+		 * @ORM\Column(type="text", nullable=true)
+		 *
+		 * @var string|null
+		 */
+		private $description = null;
+
+		/**
+		 * @ORM\Column(type="json")
+		 *
+		 * @var string[]
+		 * @see Element::DAMAGE
+		 */
+		private $elements = [];
+
+		/**
+		 * @var string[]
+		 */
+		private $resistances = [];
+
+		/**
+		 * Monster constructor.
+		 *
+		 * @param string $name
+		 * @param string $type
+		 * @param string $species
+		 */
+		public function __construct(string $name, string $type, string $species) {
+			$this->name = $name;
+			$this->type = $type;
+			$this->species = $species;
+
+			$this->ailments = new ArrayCollection();
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getName(): string {
+			return $this->name;
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getType(): string {
+			return $this->type;
+		}
+
+		/**
+		 * @param string $type
+		 *
+		 * @return $this
+		 */
+		public function setType(string $type) {
+			$this->type = $type;
+
+			return $this;
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getSpecies(): string {
+			return $this->species;
+		}
+
+		/**
+		 * @param string $species
+		 *
+		 * @return $this
+		 */
+		public function setSpecies(string $species) {
+			$this->species = $species;
+
+			return $this;
+		}
+
+		/**
+		 * @return null|string
+		 */
+		public function getDescription(): ?string {
+			return $this->description;
+		}
+
+		/**
+		 * @param null|string $description
+		 *
+		 * @return $this
+		 */
+		public function setDescription(?string $description) {
+			$this->description = $description;
+
+			return $this;
+		}
+
+		/**
+		 * @return string[]
+		 */
+		public function getElements(): array {
+			return $this->elements;
+		}
+
+		/**
+		 * @param string[] $elements
+		 *
+		 * @return $this
+		 */
+		public function setElements(array $elements) {
+			$this->elements = $elements;
+
+			return $this;
+		}
+
+		/**
+		 * @return string[]
+		 */
+		public function getResistances(): array {
+			return $this->resistances;
+		}
+
+		/**
+		 * @param string[] $resistances
+		 *
+		 * @return $this
+		 */
+		public function setResistances(array $resistances) {
+			$this->resistances = $resistances;
+
+			return $this;
+		}
+
+		/**
+		 * @return Ailment[]|Collection|Selectable
+		 */
+		public function getAilments() {
+			return $this->ailments;
+		}
+	}
---
+++ src/Game/MonsterType.php
@@ -0,0 +1,13 @@
+<?php
+	namespace App\Game;
+
+	final class MonsterType {
+		public const SMALL = 'small';
+		public const LARGE = 'large';
+
+		/**
+		 * MonsterType constructor.
+		 */
+		private function __construct() {
+		}
+	}
