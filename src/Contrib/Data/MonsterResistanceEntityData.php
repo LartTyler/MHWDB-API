d404de540d88b052409a91037d42b200e0e5acb4
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-11 19:52:58 -0400

    Add monster contrib data class and supporting classes

---
+++ src/Contrib/Data/MonsterResistanceEntityData.php
@@ -0,0 +1,120 @@
+<?php
+	namespace App\Contrib\Data;
+
+	use App\Entity\MonsterResistance;
+	use App\Utility\ObjectUtil;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+
+	/**
+	 * Class MonsterResistanceEntityData
+	 *
+	 * @package App\Contrib\Data
+	 *
+	 * @see MonsterResistance
+	 */
+	class MonsterResistanceEntityData extends AbstractEntityData {
+		/**
+		 * @var string
+		 */
+		protected $element;
+
+		/**
+		 * @var string|null
+		 */
+		protected $condition = null;
+
+		/**
+		 * MonsterResistanceEntityData constructor.
+		 *
+		 * @param string $element
+		 */
+		protected function __construct(string $element) {
+			$this->element = $element;
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getElement(): string {
+			return $this->element;
+		}
+
+		/**
+		 * @param string $element
+		 *
+		 * @return $this
+		 */
+		public function setElement(string $element) {
+			$this->element = $element;
+
+			return $this;
+		}
+
+		/**
+		 * @return null|string
+		 */
+		public function getCondition(): ?string {
+			return $this->condition;
+		}
+
+		/**
+		 * @param null|string $condition
+		 *
+		 * @return $this
+		 */
+		public function setCondition(?string $condition) {
+			$this->condition = $condition;
+
+			return $this;
+		}
+
+		/**
+		 * @return array
+		 */
+		protected function doNormalize(): array {
+			return [
+				'element' => $this->getElement(),
+				'condition' => $this->getCondition(),
+			];
+		}
+
+		/**
+		 * @param object $data
+		 *
+		 * @return void
+		 */
+		public function update(object $data): void {
+			if (ObjectUtil::isset($data, 'element'))
+				$this->setElement($data->element);
+
+			if (ObjectUtil::isset($data, 'condition'))
+				$this->setCondition($data->condition);
+		}
+
+		/**
+		 * @param object $source
+		 *
+		 * @return static
+		 */
+		public static function fromJson(object $source) {
+			$data = new static($source->element);
+			$data->condition = $source->condition;
+
+			return $data;
+		}
+
+		/**
+		 * @param EntityInterface $entity
+		 *
+		 * @return static
+		 */
+		public static function fromEntity(EntityInterface $entity) {
+			if (!($entity instanceof MonsterResistance))
+				throw static::createLoadFailedException(MonsterResistance::class);
+
+			$data = new static($entity->getElement());
+			$data->condition = $entity->getCondition();
+
+			return $data;
+		}
+	}
---
+++ src/Contrib/Data/MonsterWeaknessEntityData.php
@@ -0,0 +1,149 @@
+<?php
+	namespace App\Contrib\Data;
+
+	use App\Entity\MonsterWeakness;
+	use App\Utility\ObjectUtil;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+
+	/**
+	 * Class MonsterWeaknessEntityData
+	 *
+	 * @package App\Contrib\Data
+	 *
+	 * @see MonsterWeakness
+	 */
+	class MonsterWeaknessEntityData extends AbstractEntityData {
+		/**
+		 * @var string
+		 */
+		protected $element;
+
+		/**
+		 * @var int
+		 */
+		protected $stars;
+
+		/**
+		 * @var string|null
+		 */
+		protected $condition = null;
+
+		/**
+		 * MonsterWeaknessEntityData constructor.
+		 *
+		 * @param string $element
+		 * @param int    $stars
+		 */
+		protected function __construct(string $element, int $stars) {
+			$this->element = $element;
+			$this->stars = $stars;
+		}
+
+		/**
+		 * @return string
+		 */
+		public function getElement(): string {
+			return $this->element;
+		}
+
+		/**
+		 * @param string $element
+		 *
+		 * @return $this
+		 */
+		public function setElement(string $element) {
+			$this->element = $element;
+
+			return $this;
+		}
+
+		/**
+		 * @return int
+		 */
+		public function getStars(): int {
+			return $this->stars;
+		}
+
+		/**
+		 * @param int $stars
+		 *
+		 * @return $this
+		 */
+		public function setStars(int $stars) {
+			$this->stars = $stars;
+
+			return $this;
+		}
+
+		/**
+		 * @return null|string
+		 */
+		public function getCondition(): ?string {
+			return $this->condition;
+		}
+
+		/**
+		 * @param null|string $condition
+		 *
+		 * @return $this
+		 */
+		public function setCondition(?string $condition) {
+			$this->condition = $condition;
+
+			return $this;
+		}
+
+		/**
+		 * @return array
+		 */
+		protected function doNormalize(): array {
+			return [
+				'element' => $this->getElement(),
+				'stars' => $this->getStars(),
+				'condition' => $this->getCondition(),
+			];
+		}
+
+		/**
+		 * @param object $data
+		 *
+		 * @return void
+		 */
+		public function update(object $data): void {
+			if (ObjectUtil::isset($data, 'element'))
+				$this->setElement($data->element);
+
+			if (ObjectUtil::isset($data, 'stars'))
+				$this->setStars($data->stars);
+
+			if (ObjectUtil::isset($data, 'condition'))
+				$this->setCondition($data->condition);
+		}
+
+		/**
+		 * @param object $source
+		 *
+		 * @return static
+		 */
+		public static function fromJson(object $source) {
+			$data = new static($source->element, $source->stars);
+			$data->condition = $source->condition;
+
+			return $data;
+		}
+
+		/**
+		 * @param EntityInterface $entity
+		 *
+		 * @return static
+		 */
+		public static function fromEntity(EntityInterface $entity) {
+			if (!($entity instanceof MonsterWeakness))
+				throw static::createLoadFailedException(MonsterWeakness::class);
+
+			$data = new static($entity->getElement(), $entity->getStars());
+			$data->condition = $entity->getCondition();
+
+			return $data;
+		}
+	}
