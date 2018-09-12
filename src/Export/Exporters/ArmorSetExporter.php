96630fc9dc765db82fc3fdfb6ededfd0d11fcb26
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-12 16:31:18 -0400

    Update armor set exporter to use new contrib data class

--- src/Export/Exporters/ArmorSetExporter.php
+++ src/Export/Exporters/ArmorSetExporter.php
@@ -1,27 +1,16 @@
 <?php
 	namespace App\Export\Exporters;
 
-	use App\Entity\Armor;
+	use App\Contrib\Data\ArmorSetEntityData;
 	use App\Entity\ArmorSet;
-	use App\Entity\ArmorSetBonusRank;
 	use App\Export\Export;
-	use App\Export\ExportHelper;
 
 	class ArmorSetExporter extends AbstractExporter {
 		/**
-		 * @var ExportHelper
-		 */
-		protected $helper;
-
-		/**
 		 * ArmorSetExporter constructor.
-		 *
-		 * @param ExportHelper $helper
 		 */
-		public function __construct(ExportHelper $helper) {
+		public function __construct() {
 			parent::__construct(ArmorSet::class);
-
-			$this->helper = $helper;
 		}
 
 		/**
@@ -33,31 +22,7 @@
 			if (!($object instanceof ArmorSet))
 				throw new \InvalidArgumentException('$object must be an instance of ' . ArmorSet::class);
 
-			$helper = $this->helper;
-
-			$output = [
-				'name' => $object->getName(),
-				'rank' => $object->getRank(),
-				'pieces' => $object->getPieces()->map(function(Armor $armor) use ($helper): string {
-					return $helper->getReference($armor, 'armor.read', 'idOrSlug');
-				})->toArray(),
-			];
-
-			if ($bonus = $object->getBonus()) {
-				$output['bonus'] = [
-					'name' => $bonus->getName(),
-					'ranks' => $bonus->getRanks()->map(function(ArmorSetBonusRank $rank) use ($helper): array {
-						return [
-							'pieces' => $rank->getPieces(),
-							'skill' => $helper->toSimpleSkillRank($rank->getSkill()),
-						];
-					})->toArray(),
-				];
-
-				ksort($output['bonus']);
-			}
-
-			ksort($output);
+			$output = ArmorSetEntityData::fromEntity($object)->normalize();
 
 			return new Export('armor-sets/' . $object->getRank(), $output);
 		}
