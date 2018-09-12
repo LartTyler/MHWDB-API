1036a4d5bce668ffe36c371c8651bc34e4f0f54d
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-12 16:32:16 -0400

    Update decoration exporter to use contrib data class

--- src/Export/Exporters/DecorationExporter.php
+++ src/Export/Exporters/DecorationExporter.php
@@ -1,26 +1,16 @@
 <?php
 	namespace App\Export\Exporters;
 
+	use App\Contrib\Data\DecorationEntityData;
 	use App\Entity\Decoration;
-	use App\Entity\SkillRank;
 	use App\Export\Export;
-	use App\Export\ExportHelper;
 
 	class DecorationExporter extends AbstractExporter {
 		/**
-		 * @var ExportHelper
-		 */
-		protected $helper;
-
-		/**
 		 * DecorationExporter constructor.
-		 *
-		 * @param ExportHelper $helper
 		 */
-		public function __construct(ExportHelper $helper) {
+		public function __construct() {
 			parent::__construct(Decoration::class);
-
-			$this->helper = $helper;
 		}
 
 		/**
@@ -32,15 +22,7 @@
 			if (!($object instanceof Decoration))
 				throw new \InvalidArgumentException('$object must be an instance of ' . Decoration::class);
 
-			$output = [
-				'slug' => $object->getSlug(),
-				'name' => $object->getName(),
-				'slot' => $object->getSlot(),
-				'rarity' => $object->getRarity(),
-				'skills' => $this->helper->toSimpleSkillRankArray($object->getSkills()),
-			];
-
-			ksort($output);
+			$output = DecorationEntityData::fromEntity($object)->normalize();
 
 			return new Export('decorations', $output);
 		}
