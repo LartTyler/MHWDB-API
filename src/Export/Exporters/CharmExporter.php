5c88cbb2b9157d8ee55f840b328dfc0641462ee1
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-12 16:32:08 -0400

    Update charm exporter to use contrib data class

--- src/Export/Exporters/CharmExporter.php
+++ src/Export/Exporters/CharmExporter.php
@@ -1,27 +1,16 @@
 <?php
 	namespace App\Export\Exporters;
 
+	use App\Contrib\Data\CharmEntityData;
 	use App\Entity\Charm;
-	use App\Entity\CharmRank;
-	use App\Entity\SkillRank;
 	use App\Export\Export;
-	use App\Export\ExportHelper;
 
 	class CharmExporter extends AbstractExporter {
 		/**
-		 * @var ExportHelper
-		 */
-		protected $helper;
-
-		/**
 		 * CharmExporter constructor.
-		 *
-		 * @param ExportHelper $helper
 		 */
-		public function __construct(ExportHelper $helper) {
+		public function __construct() {
 			parent::__construct(Charm::class);
-
-			$this->helper = $helper;
 		}
 
 		/**
@@ -33,35 +22,7 @@
 			if (!($object instanceof Charm))
 				throw new \InvalidArgumentException('$object must be an instance of ' . Charm::class);
 
-			$helper = $this->helper;
-
-			$output = [
-				'slug' => $object->getSlug(),
-				'name' => $object->getName(),
-				'ranks' => $object->getRanks()->map(function(CharmRank $rank) use ($helper): array {
-					$output = [
-						'name' => $rank->getName(),
-						'level' => $rank->getLevel(),
-						'rarity' => $rank->getRarity(),
-						'skills' => $rank->getSkills()->map(function(SkillRank $rank) use ($helper): array {
-							return $helper->toSimpleSkillRank($rank);
-						})->toArray(),
-					];
-
-					if ($crafting = $rank->getCrafting()) {
-						$output['crafting'] = [
-							'craftable' => $crafting->isCraftable(),
-							'materials' => $helper->toSimpleCostArray($crafting->getMaterials()),
-						];
-					}
-
-					ksort($output);
-
-					return $output;
-				})->toArray(),
-			];
-
-			ksort($output);
+			$output = CharmEntityData::fromEntity($object)->normalize();
 
 			return new Export('charms', $output);
 		}
