dd4d95b59f0050b3e5c8e15a5046e39b6aab0f5c
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-12 16:31:04 -0400

    Update armor exporter to use armor contrib data class

--- src/Export/Exporters/ArmorExporter.php
+++ src/Export/Exporters/ArmorExporter.php
@@ -1,29 +1,18 @@
 <?php
 	namespace App\Export\Exporters;
 
+	use App\Contrib\Data\ArmorEntityData;
 	use App\Entity\Armor;
-	use App\Entity\SkillRank;
-	use App\Entity\Slot;
-	use App\Export\AssetExport;
 	use App\Export\Export;
-	use App\Export\ExportHelper;
-	use App\Game\Element;
 
 	class ArmorExporter extends AbstractExporter {
-		/**
-		 * @var ExportHelper
-		 */
-		protected $helper;
+		use AssetExporterTrait;
 
 		/**
 		 * ArmorExporter constructor.
-		 *
-		 * @param ExportHelper $helper
 		 */
-		public function __construct(ExportHelper $helper) {
+		public function __construct() {
 			parent::__construct(Armor::class);
-
-			$this->helper = $helper;
 		}
 
 		/**
@@ -35,62 +24,19 @@
 			if (!($object instanceof Armor))
 				throw new \InvalidArgumentException('$object must be an instance of ' . Armor::class);
 
-			$resistances = $object->getResistances();
-			$defense = $object->getDefense();
+			$data = ArmorEntityData::fromEntity($object);
+			$export = new Export('armor/' . $object->getRank(), $data->normalize());
 
-			$output = [
-				'slug' => $object->getSlug(),
-				'attributes' => $object->getAttributes(),
-				'name' => $object->getName(),
-				'type' => $object->getType(),
-				'rank' => $object->getRank(),
-				'rarity' => $object->getRarity(),
-				'resistances' => [
-					Element::FIRE => $resistances->getFire(),
-					Element::WATER => $resistances->getWater(),
-					Element::ICE => $resistances->getIce(),
-					Element::THUNDER => $resistances->getThunder(),
-					Element::DRAGON => $resistances->getDragon(),
-				],
-				'defense' => [
-					'base' => $defense->getBase(),
-					'max' => $defense->getMax(),
-					'augmented' => $defense->getAugmented(),
-				],
-				'skills' => $this->helper->toSimpleSkillRankArray($object->getSkills()),
-				'slots' => $object->getSlots()->map(function(Slot $slot): array {
-					return [
-						'rank' => $slot->getRank(),
-					];
-				})->toArray(),
-				'armorSet' => $this->helper->getReference($object->getArmorSet(), 'armor-sets.read'),
-			];
-
-			/** @var AssetExport[] $assetExports */
 			$assetExports = [];
 
-			if ($assets = $object->getAssets()) {
-				if ($imageFemale = $assets->getImageFemale())
-					$assetExports[] = AssetExport::fromAsset($imageFemale);
+			if ($assets = $data->getAssets()) {
+				if ($image = $assets->getImageFemale())
+					$assetExports[] = $this->createAssetExport($image);
 
-				if ($imageMale = $assets->getImageMale())
-					$assetExports[] = AssetExport::fromAsset($imageMale);
-
-				$output['assets'] = [
-					'imageFemale' => ExportHelper::toSimpleAsset($imageFemale),
-					'imageMale' => ExportHelper::toSimpleAsset($imageMale),
-				];
+				if ($image = $assets->getImageMale())
+					$assetExports[] = $this->createAssetExport($image);
 			}
 
-			if ($crafting = $object->getCrafting()) {
-				$output['crafting'] = [
-					'materials' => $this->helper->toSimpleCostArray($crafting->getMaterials()),
-				];
-			}
-
-			ksort($output);
-
-			$export = new Export('armor/' . $object->getRank(), $output);
 			$export->setAssets($assetExports);
 
 			return $export;
