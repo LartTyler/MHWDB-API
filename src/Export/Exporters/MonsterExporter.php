19c2427c1ad41dbe29d07c146f1e5d2a1053c043
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-12 16:32:45 -0400

    Update monster exporter to use contrib data class

--- src/Export/Exporters/MonsterExporter.php
+++ src/Export/Exporters/MonsterExporter.php
@@ -1,27 +1,16 @@
 <?php
 	namespace App\Export\Exporters;
 
+	use App\Contrib\Data\MonsterEntityData;
 	use App\Entity\Monster;
-	use App\Entity\MonsterResistance;
-	use App\Entity\MonsterWeakness;
 	use App\Export\Export;
-	use App\Export\ExportHelper;
 
 	class MonsterExporter extends AbstractExporter {
 		/**
-		 * @var ExportHelper
-		 */
-		protected $helper;
-
-		/**
 		 * MonsterExporter constructor.
-		 *
-		 * @param ExportHelper $helper
 		 */
-		public function __construct(ExportHelper $helper) {
+		public function __construct() {
 			parent::__construct(Monster::class);
-
-			$this->helper = $helper;
 		}
 
 		/**
@@ -33,38 +22,7 @@
 			if (!($object instanceof Monster))
 				throw new \InvalidArgumentException('$object must be an instance of ' . Monster::class);
 
-			$output = [
-				'name' => $object->getName(),
-				'type' => $object->getType(),
-				'species' => $object->getSpecies(),
-				'ailments' => $this->helper->getReferenceArray($object->getAilments(), 'ailments.read'),
-				'locations' => $this->helper->getReferenceArray($object->getLocations(), 'locations.read'),
-				'resistances' => $object->getResistances()->map(function(MonsterResistance $resistance): array {
-					$output = [
-						'element' => $resistance->getElement(),
-						'condition' => $resistance->getCondition(),
-					];
-
-					ksort($output);
-
-					return $output;
-				})->toArray(),
-				'weaknesses' => $object->getWeaknesses()->map(function(MonsterWeakness $weakness): array {
-					$output = [
-						'element' => $weakness->getElement(),
-						'stars' => $weakness->getStars(),
-						'condition' => $weakness->getCondition(),
-					];
-
-					ksort($output);
-
-					return $output;
-				})->toArray(),
-				'description' => $object->getDescription(),
-				'elements' => $object->getElements(),
-			];
-
-			ksort($output);
+			$output = MonsterEntityData::fromEntity($object)->normalize();
 
 			return new Export('monsters', $output);
 		}
