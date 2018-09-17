ed8d3246a33e71ca096cafb3be885f4dc4fbd80c
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-17 17:07:12 -0400

    Add motion value importer

---
+++ src/Import/Importers/MotionValueImporter.php
@@ -0,0 +1,46 @@
+<?php
+	namespace App\Import\Importers;
+
+	use App\Entity\MotionValue;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+
+	class MotionValueImporter extends AbstractImporter {
+		/**
+		 * MotionValueImporter constructor.
+		 */
+		public function __construct() {
+			parent::__construct(MotionValue::class);
+		}
+
+		/**
+		 * @param EntityInterface $entity
+		 * @param object          $data
+		 *
+		 * @return void
+		 */
+		public function import(EntityInterface $entity, object $data): void {
+			if (!($entity instanceof MotionValue))
+				throw $this->createCannotImportException();
+
+			$entity
+				->setName($data->name)
+				->setWeaponType($data->weaponType)
+				->setDamageType($data->damageType)
+				->setExhaust($data->exhaust)
+				->setStun($data->stun)
+				->setHits($data->hits);
+		}
+
+		/**
+		 * @param object $data
+		 *
+		 * @return EntityInterface
+		 */
+		public function create(object $data): EntityInterface {
+			$motionValue = new MotionValue($data->name, $data->weaponType);
+
+			$this->import($motionValue, $data);
+
+			return $motionValue;
+		}
+	}
