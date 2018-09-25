3d09867443e320201a3ed69bdb770147567ceb7f
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-09-25 17:33:13 -0400

    Add Ailment transformer

---
+++ src/Contrib/Transformers/AilmentTransformer.php
@@ -0,0 +1,128 @@
+<?php
+	namespace App\Contrib\Transformers;
+
+	use App\Contrib\Exceptions\ValidationException;
+	use App\Entity\Ailment;
+	use App\Entity\Item;
+	use App\Entity\Monster;
+	use App\Entity\Skill;
+	use App\Utility\ObjectUtil;
+	use DaybreakStudios\Utility\DoctrineEntities\EntityInterface;
+	use Doctrine\ORM\EntityManagerInterface;
+
+	class AilmentTransformer extends AbstractTransformer {
+		/**
+		 * @var EntityManagerInterface
+		 */
+		protected $entityManager;
+
+		/**
+		 * AilmentTransformer constructor.
+		 *
+		 * @param EntityManagerInterface $entityManager
+		 */
+		public function __construct(EntityManagerInterface $entityManager) {
+			parent::__construct($entityManager, Ailment::class);
+		}
+
+		/**
+		 * @param object $data
+		 *
+		 * @return EntityInterface
+		 */
+		public function create(object $data): EntityInterface {
+			if (!ObjectUtil::isset($data, 'name'))
+				throw ValidationException::missingField('name');
+			else if (!ObjectUtil::isset($data, 'description'))
+				throw ValidationException::missingField('description');
+
+			$ailment = new Ailment($data->name, $data->description);
+			$this->update($ailment, $data);
+
+			$this->entityManager->persist($ailment);
+
+			return $ailment;
+		}
+
+		/**
+		 * @param EntityInterface $entity
+		 * @param object          $data
+		 *
+		 * @return void
+		 */
+		public function update(EntityInterface $entity, object $data): void {
+			if (!($entity instanceof Ailment))
+				throw $this->createEntityNotSupportedException();
+
+			if (ObjectUtil::isset($data, 'name'))
+				$entity->setName($data->name);
+
+			if (ObjectUtil::isset($data, 'description'))
+				$entity->setDescription($data->description);
+
+			if (ObjectUtil::isset($data, 'recovery')) {
+				$recovery = $entity->getRecovery();
+				$definition = $data->recovery;
+
+				if (ObjectUtil::isset($definition, 'items')) {
+					$this->populateFromIdArray(
+						'recovery.items',
+						$recovery->getItems(),
+						Item::class,
+						$definition->items
+					);
+				}
+
+				if (ObjectUtil::isset($definition, 'actions'))
+					$recovery->setActions($definition->actions);
+			}
+
+			if (ObjectUtil::isset($data, 'protection')) {
+				$protection = $entity->getProtection();
+				$definition = $data->protection;
+
+				if (ObjectUtil::isset($definition, 'items')) {
+					$this->populateFromIdArray(
+						'protection.items',
+						$protection->getItems(),
+						Item::class,
+						$definition->items
+					);
+				}
+
+				if (ObjectUtil::isset($definition, 'skills')) {
+					$this->populateFromIdArray(
+						'protection.skills',
+						$protection->getSkills(),
+						Skill::class,
+						$definition->skills
+					);
+				}
+			}
+		}
+
+		/**
+		 * @param EntityInterface $entity
+		 *
+		 * @return void
+		 */
+		public function delete(EntityInterface $entity): void {
+			if (!($entity instanceof Ailment))
+				throw $this->createEntityNotSupportedException();
+
+			/** @var Monster[] $monsters */
+			$monsters = $this->entityManager->createQueryBuilder()
+				->from(Monster::class, 'm')
+				->leftJoin('m.ailments', 'a')
+				->select('m')
+				->where('a.id = :ailment')
+				->setParameter('ailment', $entity)
+				->getQuery()
+				->getResult();
+
+			foreach ($monsters as $monster)
+				$monster->getAilments()->removeElement($entity);
+
+			$this->entityManager->remove($entity);
+		}
+	}
