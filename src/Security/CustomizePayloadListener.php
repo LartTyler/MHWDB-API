12c90ad944f90e96df7f3b9c7a22548633fc6143
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-04-13 21:22:35 -0400

    Add user's display name to JWT payload

---
+++ src/Security/CustomizePayloadListener.php
@@ -0,0 +1,24 @@
+<?php
+	namespace App\Security;
+
+	use App\Entity\User;
+	use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+	class CustomizePayloadListener {
+		/**
+		 * @param JWTCreatedEvent $event
+		 *
+		 * @return void
+		 */
+		public function onJwtCreate(JWTCreatedEvent $event): void {
+			$user = $event->getUser();
+
+			if (!($user instanceof User))
+				return;
+
+			$payload = $event->getData();
+			$payload['displayName'] = $user->getDisplayName();
+
+			$event->setData($payload);
+		}
+	}
