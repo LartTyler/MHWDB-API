4d8381b987e7c4e10a1955108950cd83c64503bf
Tyler Lartonoix <lord.codemonkey@gmail.com>
2020-02-05 17:27:18 -0500

    Add support for purple sharpness

--- src/Game/Sharpness.php
+++ src/Game/Sharpness.php
@@ -1,26 +1,16 @@
 <?php
 	namespace App\Game;
 
+	use DaybreakStudios\RestApiCommon\Utility\ConstantsClassTrait;
+
 	final class Sharpness {
+		use ConstantsClassTrait;
+
 		const RED = 'red';
 		const ORANGE = 'orange';
 		const YELLOW = 'yellow';
 		const GREEN = 'green';
 		const BLUE = 'blue';
 		const WHITE = 'white';
-
-		const ALL = [
-			self::RED,
-			self::ORANGE,
-			self::YELLOW,
-			self::GREEN,
-			self::BLUE,
-			self::WHITE,
-		];
-
-		/**
-		 * Sharpness constructor.
-		 */
-		private function __construct() {
-		}
+		const PURPLE = 'purple';
 	}
