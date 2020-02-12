55032f156166deaaeb396d4ddedf6b57ef315ac8
Tyler Lartonoix <lord.codemonkey@gmail.com>
2020-02-12 11:12:54 -0500

    Add migration

---
+++ src/Migrations/Version20200211231809.php
@@ -0,0 +1,37 @@
+<?php
+	declare(strict_types=1);
+
+	namespace DoctrineMigrations;
+
+	use Doctrine\DBAL\Schema\Schema;
+	use Doctrine\Migrations\AbstractMigration;
+
+	/**
+	 * Auto-generated Migration: Please modify to your needs!
+	 */
+	final class Version20200211231809 extends AbstractMigration {
+		public function getDescription(): string {
+			return '';
+		}
+
+		public function up(Schema $schema): void {
+			// this up() migration is auto-generated, please modify it to your needs
+			$this->abortIf(
+				$this->connection->getDatabasePlatform()->getName() !== 'mysql',
+				'Migration can only be executed safely on \'mysql\'.'
+			);
+
+			$this->addSql('ALTER TABLE items ADD buy_price INT UNSIGNED NOT NULL, ADD sell_price INT UNSIGNED NOT NULL');
+			$this->addSql('UPDATE items SET sell_price = _value');
+		}
+
+		public function down(Schema $schema): void {
+			// this down() migration is auto-generated, please modify it to your needs
+			$this->abortIf(
+				$this->connection->getDatabasePlatform()->getName() !== 'mysql',
+				'Migration can only be executed safely on \'mysql\'.'
+			);
+
+			$this->addSql('ALTER TABLE items DROP buy_price, DROP sell_price');
+		}
+	}
